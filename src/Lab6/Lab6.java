6866e74b84d2865c8f19444b72472a4b2f7eec8c
JoRanda <jorandachapman@gmail.com>
2018-08-24 23:07:25 -0400

    Update

--- src/Lab6/Lab6.java
+++ src/Lab6/Lab6.java
@@ -8,29 +8,34 @@ public class Lab6 {
 	public static void main(String[] args) {
 		Scanner scnr = new Scanner(System.in);
         Random  rand = new Random();		
-        String choice;
+        String choice = "y";
+        int i=1; 
    
      System.out.println("Welcome to Grand Circus Casino!");
-     System.out.println();
-    
+     
+     // User Input on how many sides of the die
      System.out.println("How many sides should each die have?");
-       int sides = scnr.nextInt();  //Prompt for User Input 
+     int sides = scnr.nextInt();  //Prompt for User Input 
        
-  do {
+     while (choice.equalsIgnoreCase("y")) {
    
 	 System.out.println(roll (sides));
-	 
+	
 	 int die1 = (int)(Math.random()*6) +1;
      int die2 = (int)(Math.random()*6) +1;
     
      System.out.println("Roll 1:");
      System.out.println(die1 + "\n" + die2);
-    
+     
      System.out.print("Roll again (y/n): ");
      choice = scnr.next();
      System.out.println(); 
+     
+     ++i; 
 
-} while(choice.equalsIgnoreCase("y"));
+    }
+     
+     System.out.println("Goodbye, Thanks for Playing!");
   
 	}
 
@@ -39,9 +44,10 @@ public class Lab6 {
 		rand.nextInt(sides +1);
 		return sides;
 	}
+	
 	public static String callSpecial(int die1,int die2) {
 		
-		String = specialRoll; 
+		int specialRoll; 
 
 	if (die1 == 1 && die2 == 1) {
 		 
@@ -55,9 +61,12 @@ public class Lab6 {
 		
 		return "Craps!";
 		 
-	}
-	}
-	}
+}
+	return "";
+	      }
+	
+
+ }
 
 
 	
